a7c7718414b6fb36c40fc41fa8572f8593fc0dd4
stepoleggg <olegstefanoyv1@gmail.com>
2019-08-02 14:33:49 +0500

    fix bugs

---
+++ make_video.py
@@ -0,0 +1,23 @@
+import cv2
+import os
+import re
+
+def make_video(files, folder, name, fps=24):
+    """
+        Создает видео из изображений в папке data\predict
+    """
+    out = cv2.VideoWriter(f"video\{name}.mp4", cv2.VideoWriter_fourcc(*'mp4v'), fps, (1280, 720))
+    #print(files)
+    files.sort(key = lambda x: int(re.search(r'\d+', x).group()))
+    for file in files:
+        print(f"data\predict\{folder}\{file}")
+        out.write(cv2.imread(f"data\predict\{folder}\{file}"))
+    out.release() 
+    cv2.destroyAllWindows()
+    print(f"Видео сохранено video\{name}.mp4")
+
+folder = input("Введите название папки с png в data\predict\: ")
+files = os.listdir(path=os.getcwd()+f"\data\predict\{folder}")
+fps = input("Введите fps: ")
+name = input("Введите имя видео для сохранения: ")
+make_video(files, folder, name, fps=int(fps))
